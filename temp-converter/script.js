c85f7729ad21f080e2513760d40c1871f66f53bc
Suneel Mekala <suneelsweety31@gmail.com>
2023-09-30 07:49:06 +0530

    first commit

---
+++ temp-converter/script.js
@@ -0,0 +1,26 @@
+const celsiusEl = document.getElementById("Celcius");
+const FahrenheitEl = document.getElementById("Fahrenheit");
+const kelvinEl = document.getElementById("Kelvin");
+
+function computeTemp() {
+  //Add + to avoid String Problem
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "Celcius":
+      kelvinEl.value = currentValue + 273.15;
+      FahrenheitEl.value = currentValue * 1.8 + 32;
+      break;
+    case "Fahrenheit":
+      celsiusEl.value = (currentValue - 32) / 1.8;
+      kelvinEl.value = (currentValue -32) / 1.8 + 273.15;
+      break;
+    case "Kelvin":
+      celsiusEl.value = currentValue - 273.15;
+      FahrenheitEl.value = (currentValue - 273.15) * 1.8 + 32;
+      break;
+
+    default:
+      break;
+  }
+}
